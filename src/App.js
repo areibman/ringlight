e208be732e095342f1f41d95bff55b9646d98cf8
reibs <areibman@gmail.com>
2021-07-23 18:18:01 -0700

    movable

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import styled from "styled-components";
+import Webcam from "react-webcam";
+import Draggable from "react-draggable";
+import Switch from "react-switch";
+
+const Container = styled.div`
+  height: 100%;
+  width: 100%;
+`;
+
+const RingOutter = styled.span`
+  height: 100vh;
+  width: 100vw;
+  background-color: white;
+  /* border-radius: 50%; */
+  position: absolute;
+  z-index: 0;
+`;
+
+const RingInner = styled.span`
+  height: 80vh;
+  width: 80vh;
+  background-color: black;
+  border-radius: 50%;
+  position: absolute;
+  z-index: 1;
+`;
+
+const WebcamBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: green;
+  position: absolute;
+  z-index: 3;
+`;
+
+const LightSwitch = styled(Switch)`
+  position: absolute;
+  z-index: 3;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <div className="App">
+        <header className="App-header">
+          <Draggable>
+            <WebcamBox>I can now be moved around!</WebcamBox>
+          </Draggable>
+          <RingOutter />
+          <RingInner />
+        </header>
+      </div>
+    </Container>
   );
 }
 
